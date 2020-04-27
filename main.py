cf45da711dcab2c20a08ace1c949a354f6e27f0c
Adrigarr <sgadrian@hotmail.com>
2020-04-27 15:12:30 +0200

    Heroku funcional
    Se hacen cambios para que la aplicación se sirva correctamente en Heroku.

--- main.py
+++ main.py
@@ -64,9 +64,10 @@ async def test(request):
     return text(relationsDF)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(
-        host="0.0.0.0",
-        port=os.environ.get('PORT') or 8000,
-        debug=True
-    ) 
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 8000)),
+        workers=int(os.environ.get('WEB_CONCURRENCY', 1)),
+        debug=bool(os.environ.get('DEBUG', ''))
+    )
