29de4e20162439b9c86dd462bad4d33a2521c5a2
Adrigarr <sgadrian@hotmail.com>
2020-04-27 21:35:19 +0200

    Jinja2 y nuevo HTML
    - Se añade una nueva plantilla sencilla.
    
    - Se utiliza Jinja2 para manejar plantillas. De esta nueva forma se pueden introducir valores para las variables de los HTML de forma sencilla.

--- main.py
+++ main.py
@@ -5,7 +5,22 @@ import getInfoSongs
 
 from sanic import Sanic
 from sanic.response import json, html, text
+from jinja2 import Environment, PackageLoader, select_autoescape
 import os
+
+
+# define the environment for the Jinja2 templates
+env = Environment(
+    loader=PackageLoader('main', 'templates'),
+    autoescape=select_autoescape(['html', 'xml', 'tpl'])
+)
+
+
+# a function for loading an HTML template from the Jinja environment
+def template(tpl, **kwargs):
+    template = env.get_template(tpl)
+    return html(template.render(kwargs))
+
 
 LOG_FILE = datetime.now().strftime("%Y%m%d") + "logfile.log"
 
@@ -54,7 +69,7 @@ async def test(request):
     template = open(os.getcwd() + "/templates/index.html")
     return html(template.read())
 
-# Esta es la ruta en la que se muestra nuestro estudio
+# Esta es la ruta en la que se muestra nuestro estudio sin html
 @app.route("/run")
 async def test(request):
     song1='Q158553' #satisfaction rolling stones
@@ -63,6 +78,24 @@ async def test(request):
 
     return text(relationsDF)
 
+
+# Esta ruta muestra un html diferente que muestra las variables
+# que le indiquemos.
+# Esta será la forma preferible de mostrar las páginas
+@app.route('/show')
+async def test(request):
+    song1='Q158553' #satisfaction rolling stones
+    song2='Q607742' #hey the beatles
+    relationsDF = main(song1,song2) # relationsDF es un DataFrame
+
+    aux = 'PROBANDO, PROBANDO'
+    data = relationsDF
+
+    return template(
+        'index2.html',
+        aux=aux,
+        data=data
+    )
 
 if __name__ == '__main__':
     app.run(
